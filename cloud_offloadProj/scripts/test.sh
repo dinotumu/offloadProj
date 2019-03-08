3d2efa86995e0d70df1c5897b7a75edc3fffb004
dinoRkz <saidinesh_tumu@outlook.com>
2019-03-08 20:17:37 +0000

    Added test script to test the complete working of the container

--- cloud_offloadProj/scripts/test.sh
+++ cloud_offloadProj/scripts/test.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+docker ps -f name=tesseract-cn
+TMP=TMP_$$_$(date +"%N")
+docker exec -it tesseract_container mkdir \-p ./$TMP/
+docker cp ./input/image.jpg tesseract_container:/home/work/$TMP/
+docker exec -it tesseract_container /bin/ash -c "cd ./$TMP/; tesseract ./image.jpg $TMP"
+docker cp tesseract_container:/home/work/$TMP/$TMP.txt ./output/
+docker exec -it tesseract_container rm \-r ./$TMP/
+docker exec -it tesseract-cn ls
