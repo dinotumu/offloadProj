2330edd7c381b42638e2a5a1571f513864cd782d
dinoRkz <saidinesh_tumu@outlook.com>
2019-03-08 20:39:50 +0000

    Sample image; Fixed test.sh; quick_test.sh to run complete app

--- cloud_offloadProj/scripts/quick_test.sh
+++ cloud_offloadProj/scripts/quick_test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-docker ps -f name=tesseract-cn
+docker run -dt --name tesseract-cn tesseract
 TMP=TMP_$$_$(date +"%N")
 docker exec -it tesseract-cn mkdir \-p ./$TMP/
 docker cp ./input/image.jpg tesseract-cn:/home/work/$TMP/
 docker exec -it tesseract-cn /bin/ash -c "cd ./$TMP/; tesseract ./image.jpg $TMP"
 docker cp tesseract-cn:/home/work/$TMP/$TMP.txt ./output/
 docker exec -it tesseract-cn rm \-r ./$TMP/
-docker exec -it tesseract-cn ls
+docker stop tesseract-cn
+docker rm tesseract-cn
