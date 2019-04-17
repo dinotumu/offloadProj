a4e030a2e4b0b0bc727aa55338c9b13fb5020ded
dinoRkz <saidinesh_tumu@outlook.com>
2019-04-17 18:46:19 +0100

    more refractoring ..

--- docker/main.sh
+++ docker/main.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+PWD='/home/dinotumu/Documents/offloadProj/cloud_offloadProj'
+FILE_NAME='image.png'
+FILE_PATH=$PWD'/data/input/'$FILE_NAME
+OUTPUT_DIR=$PWD'/data/output/'
+TMP=TMP_$$_$(date +"%N")
+
+
+docker run -dt --name tesseract-cn tesseract
+docker exec -it tesseract-cn mkdir \-p ./$TMP/
+docker cp $FILE_PATH tesseract-cn:/home/app/$TMP/
+docker exec -it tesseract-cn /bin/ash -c "cd ./$TMP/; tesseract $FILE_NAME $TMP"
+docker cp tesseract-cn:/home/app/$TMP/$TMP.txt $OUTPUT_DIR
+docker exec -it tesseract-cn rm \-r ./$TMP/
+docker stop tesseract-cn
+docker rm tesseract-cn
