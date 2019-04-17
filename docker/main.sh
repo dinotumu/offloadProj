715d3257d07ade5da6c3ff1f99978a9a24e73ad9
dinoRkz <saidinesh_tumu@outlook.com>
2019-04-17 18:59:28 +0100

    'docker' folder changes done

--- docker/main.sh
+++ docker/main.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-PWD='/home/dinotumu/Documents/offloadProj/cloud_offloadProj'
+# define constants
+PWD=$(pwd)'/docker'
 FILE_NAME='image.png'
 FILE_PATH=$PWD'/data/input/'$FILE_NAME
 OUTPUT_DIR=$PWD'/data/output/'
 TMP=TMP_$$_$(date +"%N")
 
-
+# start the 'tesseract-cn' container using the customised 'tesseract docker alpine linux' image
 docker run -dt --name tesseract-cn tesseract
+
+# copy input image to shared volume
 docker exec -it tesseract-cn mkdir \-p ./$TMP/
 docker cp $FILE_PATH tesseract-cn:/home/app/$TMP/
+
+# execute the input using 'tesseract-cn' docker container
 docker exec -it tesseract-cn /bin/ash -c "cd ./$TMP/; tesseract $FILE_NAME $TMP"
+
+# get the output from the shared volume
 docker cp tesseract-cn:/home/app/$TMP/$TMP.txt $OUTPUT_DIR
 docker exec -it tesseract-cn rm \-r ./$TMP/
+
+# stop and remove docker container
 docker stop tesseract-cn
 docker rm tesseract-cn
